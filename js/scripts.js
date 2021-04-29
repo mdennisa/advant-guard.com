8bcddbc0b0bad1bc8f8854e8165817f029d46440
muhammad dennisa <mdennisa@gmail.com>
2021-04-29 13:51:29 +0800

    content updates, add logo and slide-4

--- js/scripts.js
+++ js/scripts.js
@@ -38,27 +38,12 @@ jQuery(function($) {
         targets: '#story',
         duration: 400,
         easing: 'easeOutSine',
-        translateX: [-100, 0],
+        translateY: [100, 0],
         opacity: [0, 1]
-    })
-    .add({
-        targets: '#story h2',
-        duration: 400,
-        easing: 'easeOutExpo',
-        translateX: [-100, 0],
-        opacity: [0, 1],
-    })
-    .add({
-        targets: '#story p',
-        duration: 400,
-        easing: 'easeOutExpo',
-        translateX: [-100, 0],
-        opacity: [0, 1],
-        delay: anime.stagger(100)
     })
     var scene_2 = new ScrollMagic.Scene({
         triggerElement: '#story',
-        duration: 400,
+        duration: '30%',
     })
     .setAnime(tl_s2)
     .addTo(controller)
@@ -66,8 +51,14 @@ jQuery(function($) {
     // #slide-3
     var tl_s3 = anime.timeline()
     tl_s3.add({
+        targets: '#detailing',
+        duration: 600,
+        easing: 'easeOutSine',
+        translateY: [100, 0],
+        opacity: [0, 1]
+    })
+    .add({
         targets: '#tint-specs div',
-        duration: 1200,
         easing: 'easeOutExpo',
         translateX: [100, 0],
         opacity: [0, 1],
@@ -76,11 +67,28 @@ jQuery(function($) {
 
     var scene_3 = new ScrollMagic.Scene({
         triggerElement: '#slide-3',
-        duration: '80%'
+        duration: '40%'
     })
     .setAnime(tl_s3)
     .addTo(controller)
 
+    // #slide-4
+    var tl_s4 = anime.timeline()
+    tl_s4.add({
+        targets: '#contact-us',
+        duration: 600,
+        easing: 'easeOutSine',
+        opacity: [0, 1]
+    })
+
+    var scene_4 = new ScrollMagic.Scene({
+        triggerElement: '#slide-4',
+        duration: '30%'
+    })
+    .setAnime(tl_s4)
+    .addTo(controller)
+
+    // fancybox
     $('a[data-fancybox]').fancybox({
         hideScrollbar: false
     })
